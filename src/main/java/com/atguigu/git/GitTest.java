8a4df3fc99fb66678f726bd48a6e2ea21987d47c
JS <JS@atguigu.com>
2021-06-29 18:10:00 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yhm
+ * @create 2021-06-29 16:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
